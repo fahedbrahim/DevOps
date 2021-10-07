6e37d13ab6f6534909f771933aabc71f61f343f3
moetaz.brayek <moetaz.brayek@esprit.tn>
2021-10-07 22:26:15 +0000

    :ok_hand: log4 info level

--- src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
@@ -21,6 +21,7 @@ import tn.esprit.spring.entities.Timesheet;
 import tn.esprit.spring.services.IEmployeService;
 import tn.esprit.spring.services.IEntrepriseService;
 import tn.esprit.spring.services.ITimesheetService;
+import org.apache.log4j.Logger;
 
 @RestController
 public class RestControlEmploye {
@@ -32,14 +33,21 @@ public class RestControlEmploye {
 	@Autowired
 	ITimesheetService itimesheetservice;
 
-	
+	// loger Moetaz Brayek
+	private static Logger logger = Logger.getLogger(RestControlEmploye.class);
+
+
 	// http://localhost:8081/SpringMVC/servlet/ajouterEmployer
 	//{"id":1,"nom":"kallel", "prenom":"khaled", "email":"Khaled.kallel@ssiiconsulting.tn", "isActif":true, "role":"INGENIEUR"}
 	
 	@PostMapping("/ajouterEmployer")
 	@ResponseBody
 	public Employe ajouterEmploye(@RequestBody Employe employe)
-	{
+	{	    
+		logger.info("\n----------Moetaz Brayek----------\n");
+	    logger.info("\n Trying to add employe! \n");
+		logger.info("\n--------------------\n");
+
 		iemployeservice.ajouterEmploye(employe);
 		return employe;
 	}
